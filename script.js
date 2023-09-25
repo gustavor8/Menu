7f3fd08f15f474a01301c5928caddb74556ca5f4
Gustavo Rodrigues <gustavogrmc@gmail.com>
2023-09-25 15:49:28 -0300

    código após Gabriel deixar automatico

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const button = document.getElementById("button");
-let extend = true;
+const menuButton = document.getElementById('menu-button');
+const container = document.querySelector('.container');
+const menuItems = document.querySelectorAll('.menu-item');
 
-button.addEventListener('click', () => {
-  const container = document.getElementById("container");
-  container.style.width = isExtend(extend);
-  extend = !extend;
+let isExpand = true;
+
+menuButton.addEventListener('click', () => {
+  if(isExpand){
+    container.classList.add('expand');
+  }else{
+    container.classList.remove('expand');
+  }
+  
+  isExpand = !isExpand
+})
+
+
+menuItems.forEach((menuItem) => {
+  let isArrowExpand = false;
+
+  menuItem.addEventListener('click', () => {
+    const arrow = menuItem.children[0].children[2];
+  
+    if (isArrowExpand) {
+      menuItem.children[1].style.display = 'block';
+      arrow.classList.add('arrow-expand');
+    } else {
+      menuItem.children[1].style.display = 'none';
+      arrow.classList.remove('arrow-expand');
+    }
+    isArrowExpand = !isArrowExpand;
+
+  });
 });
-
-const isExtend = (extend) => {
-  return extend ? '5vw' : '16vw'
-}
